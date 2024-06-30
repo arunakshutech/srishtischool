d41ddf3aeb84e93a3901ce7c7d921534341a6263
arunakshutech <arunkumarpython40@gmail.com>
2024-06-30 16:54:02 +0530

    upfil vercel.json

--- api/settings.py
+++ api/settings.py
@@ -122,7 +122,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'example/static'),
+    os.path.join(BASE_DIR, 'example/static/'),
 ]
 
 # Media files (Uploaded by users)
