82058b5a8acb8cc62373106af7b91e53c006432a
arunakshutech <arunkumarpython40@gmail.com>
2024-06-30 20:31:51 +0530

    newupdatel.json

--- staticfiles_build/static/example/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    // Toggle the active class to show/hide nav links
-    navLinks.classList.toggle('active');
-    burger.classList.toggle('active');
-});
