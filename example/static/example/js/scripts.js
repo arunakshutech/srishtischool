fbd4c43f36ba4fd4f71e319a015db389dfd369cd
arunakshutech <arunkumarpython40@gmail.com>
2024-06-30 20:03:00 +0530

    newupdate vercel.json

--- example/static/example/js/scripts.js
+++ example/static/example/js/scripts.js
@@ -9,3 +9,30 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleButton.classList.toggle('active');
     });
 });
+
+// carousel.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const carouselSlide = document.querySelector('.carousel-slide');
+    const carouselImages = document.querySelectorAll('.carousel-image');
+
+    let counter = 0;
+    let size = carouselImages[0].clientWidth;
+
+    function moveCarousel() {
+        if (counter >= carouselImages.length) {
+            counter = 0;
+        }
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+        counter++;
+    }
+
+    // Automatic sliding
+    setInterval(moveCarousel, 3000); // Change slide every 3 seconds
+
+    // Add responsive support
+    window.addEventListener('resize', () => {
+        size = carouselImages[0].clientWidth;
+        carouselSlide.style.transform = `translateX(${-size * counter+5}px)`;
+    });
+});
