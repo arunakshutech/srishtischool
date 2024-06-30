cadedfa10467dafe5c310779098dcb6c9d91186c
arunakshutech <arunkumarpython40@gmail.com>
2024-06-30 15:53:19 +0530

    upfil vercel.json

--- api/settings.py
+++ api/settings.py
@@ -120,7 +120,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'example/static'),
 ]
