cf7718db1f252c2c103bf896073dca5c0864b6b0
Teddyarun56 <92975943+Teddyarun56@users.noreply.github.com>
2024-06-30 11:36:00 +0530

    loaded static and media files

--- api/settings.py
+++ api/settings.py
@@ -112,8 +112,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- example/views.py
+++ example/views.py
@@ -1,16 +1,8 @@
 # example/views.py
 from datetime import datetime
+from django.shortcuts import redirect,render
+from django.template import Template
 
-from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request,'index.html')
