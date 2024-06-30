de162f05adfaf523eb2e22f629f220250f01982d
arunakshutech <arunkumarpython40@gmail.com>
2024-06-30 12:58:43 +0530

    configured style and script

--- api/settings.py
+++ api/settings.py
@@ -121,11 +121,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'example/static/'),
 ]
 
 # Media files (Uploaded by users)
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
--- api/urls.py
+++ api/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
     path('', include('example.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
